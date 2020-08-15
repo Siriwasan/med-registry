3a281df9bda771a52f54ca16dd9a8ef3a7258d19
Siriwasan <siriwasan@gmail.com>
2020-08-15 09:29:07 +0700

    add Angular Material and Angular Flex layout

---
+++ projects/med-registry/src/app/shared/shared.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MaterialModule } from './material.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+// import { FabSpeedDialModule } from './modules/fab-speed-dial/fab-speed-dial.module';
+// import { NoSanitizePipe } from './pipes/no-sanitize.pipe';
+// import { ModalDialogComponent } from './components/modal-dialog/modal-dialog.component';
+// import { IsoDatePipe } from './pipes/iso-date.pipe';
+
+@NgModule({
+  declarations: [
+    // ModalDialogComponent,
+    // NoSanitizePipe,
+    // IsoDatePipe
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    // FabSpeedDialModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    // FabSpeedDialModule,
+    // ModalDialogComponent,
+    // NoSanitizePipe,
+    // IsoDatePipe,
+  ],
+})
+export class SharedModule {}
